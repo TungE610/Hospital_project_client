5ed01f7c576b070053dcfe0fd1cc46468dd0637b
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-08-04 01:29:46 +0700

    update

--- src/pages/doctors/Doctors.jsx
+++ src/pages/doctors/Doctors.jsx
@@ -23,7 +23,7 @@ const Doctors = () => {
   const getDoctors = async () => {
 		setLoading(true)
 			try {
-				const response = await fetch("http://localhost:5000/doctors")
+				const response = await fetch("https://hospital-project-api.herokuapp.com/api/doctors")
 				const jsonData = await response.json()
 				setDoctorData(jsonData)
 				setLoading(false)
