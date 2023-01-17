f60c4570c1250474278762fac5aba06fc4386199
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-18 04:48:30 +0700

    refactor

--- src/components/navbar/logo/Logo.jsx
+++ src/components/navbar/logo/Logo.jsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import styles from './logo.module.css'
 
 const Logo = () => {
+	
   return (	
 	<div className={styles.logo}>
 		<img src={require('../../../assets/first-aid-kit.png')} className={styles.logoImg}/>
-					<div className={styles.logoName}>
-						<div className={styles.logoName1}>
-								<p className={styles.logo1Content}>Bách Khoa</p>
-					 </div>
-						<div className={styles.logoName2}><p>Clinic</p></div>
-					</div>
+			<div className={styles.logoName}>
+				<div className={styles.logoName1}>
+					<p className={styles.logo1Content}>Bách Khoa</p>
+				</div>
+			<div className={styles.logoName2}><p>Clinic</p></div>
+		</div>
 	</div>
   );
 }
