21ef3acbdf97384378cfbbfa7d9f9b8166e7bcd6
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-18 05:06:44 +0700

    refactor code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import {React, Fragment, useState, useContext, useEffect} from "react";
+import {React, useContext} from "react";
 import styles from './navbar.module.css';
 import Logo from "./logo/Logo";
 import NavbarItem from "./navbarItem/NavbarItem";
-import { Button, Modal } from "antd";
+import { Button } from "antd";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import LogginContext from '../accountBox/LogginContext'
@@ -19,46 +19,50 @@ const MyButton = styled(Button)`
 		border-radius: 10px;
   }
 `;
-const Navbar = (props) => {
-  let navigate = useNavigate();
-	const [loginData, setLoggedIn] = useContext(LogginContext);
-	const openModalHandler = (state) =>  {
-      props.openModal(state)
-	}
-const logoutHandler = () => {
-	// 	window.localStorage.setItem('isLoggedIn', false)
-	setLoggedIn({isLoggedIn : false, role:'', doctor_id : null, room_id : null})
-	navigate('/Login')
-	sessionStorage.clear();
 
-}
-// useEffect(()=> {
-// 		setIsLoggedIn(props.isLoggedIn)
-// }, [])
-return (
+const Navbar = props => {
+
+  	let navigate = useNavigate();
+
+    const [loginData, setLoggedIn] = useContext(LogginContext);
+
+	const openModalHandler = state =>  {
+      props.openModal(state);
+	};
+
+const logoutHandler = () => {
+
+	setLoggedIn({isLoggedIn : false, role:'', doctor_id : null, room_id : null});
+
+	navigate('/Login');
+
+	sessionStorage.clear();
+};
+
+	return (
 		<div className = {styles.navbar}>
 			<Logo />
-			   <NavbarItem className="navbarItem" link="/TopPage" iconLink='home.png' itemContent='HOME'/>
-				 <NavbarItem className="navbarItem" link="/AboutPage" iconLink='about.png' itemContent='ABOUT'/>
-				 {loginData.role === 'super_admin' || loginData.role === 'doctor' || sessionStorage.getItem("email") ? <NavbarItem className="navbarItem" link="/Appointments" iconLink='appointment.png' itemContent='APPOINTMENT'/> : ''}
-				 {/* <NavbarItem className="navbarItem" link="/Appointments" iconLink='dining-table.png' itemContent='Appointment'/> */}
-				 <NavbarItem className="navbarItem" link="/Doctors" iconLink='doctor.png' itemContent='DOCTORS'/>
-				 <NavbarItem className="navbarItem" link="/Rooms" iconLink='dining-table.png' itemContent='ROOMS'/>
-				 <NavbarItem className="navbarItem" link="/Contact" iconLink='contacts.png' itemContent='CONTACTS'/>
-		<div className={styles.navbarButtons}>
-			{
-				(loginData.isLoggedIn || sessionStorage.getItem("email")) ? 
-			<MyButton  type="primary" size="large" onClick={logoutHandler}>
-				<p className={styles.loginText} style={{marginBottom : 0, fontWeight : "bold"}}>Logout</p>
-			</MyButton> : 
-			<MyButton  type="primary" size="large" onClick={() => {navigate('/Login')}}>
-				<p className={styles.loginText} style={{marginBottom : 0, fontWeight : "bold"}}>Doctor Login</p>
-			</MyButton>
-			}
-			<MyButton  type="primary" size="large" onClick={() => {openModalHandler(true)}}>
-				<p className={styles.appointmentText} style={{marginBottom : 0, fontWeight : "bold"}}>Appointment</p>
-			</MyButton>
-		</div>
+				<NavbarItem className="navbarItem" link="/TopPage" iconLink='home.png' itemContent='HOME'/>
+				<NavbarItem className="navbarItem" link="/AboutPage" iconLink='about.png' itemContent='ABOUT'/>
+				{loginData.role === 'super_admin' || loginData.role === 'doctor' || sessionStorage.getItem("email") ? <NavbarItem className="navbarItem" link="/Appointments" iconLink='appointment.png' itemContent='APPOINTMENT'/> : ''}
+				<NavbarItem className="navbarItem" link="/Doctors" iconLink='doctor.png' itemContent='DOCTORS'/>
+				<NavbarItem className="navbarItem" link="/Rooms" iconLink='dining-table.png' itemContent='ROOMS'/>
+				<NavbarItem className="navbarItem" link="/Contact" iconLink='contacts.png' itemContent='CONTACTS'/>
+			<div className={styles.navbarButtons}>
+				{(loginData.isLoggedIn || sessionStorage.getItem("email")) ? 
+				<MyButton  type="primary" size="large" onClick={logoutHandler}>
+					<p className={styles.loginText} style={{marginBottom : 0, fontWeight : "bold"}}>Logout</p>
+				</MyButton>
+				: 
+				<MyButton  type="primary" size="large" onClick={() => {navigate('/Login')}}>
+					<p className={styles.loginText} style={{marginBottom : 0, fontWeight : "bold"}}>Doctor Login</p>
+				</MyButton>
+				}
+
+				<MyButton  type="primary" size="large" onClick={() => {openModalHandler(true)}}>
+					<p className={styles.appointmentText} style={{marginBottom : 0, fontWeight : "bold"}}>Appointment</p>
+				</MyButton>
+			</div>
 		</div>
 	)
 }
